0f8648478acb4ea6e9b3e704e287bbc65a96e571
Rodrigo301239 <rodrigoifpr1@gmail.com>
2026-04-17 17:19:12 -0300

    login/cadastro e index feitas, fazer home

--- Site/xcarlos/app.py
+++ Site/xcarlos/app.py
@@ -8,23 +8,34 @@ import database
 def index():
     return render_template('index.html')
 
-@app.route('/cadastrar',methods = ["GET","POST"])
+@app.route('/cadastro',methods = ["GET","POST"])
 def cadastrar():
     form = request.form
     if database.cadastro(form) == True:
-        return render_template('index.html')
+        return render_template('home.html')
     
     else:
         return ("erro1")
 
-@app.route('/login', methods=["POST"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    form = request.form
-    if database.login(form) == True:
-        session['email'] = form['email']
-        return redirect(url_for('home'))
-    else:
-        return ("erro2")
+    if request.method == "POST":
+        form = request.form
+
+        if database.login(form) == True:
+            session['email'] = form['email']
+            return redirect(url_for('home'))
+        else:
+            return "erro2"
+
+    # 👇 quando acessar pelo link (GET)
+    return render_template('login.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
